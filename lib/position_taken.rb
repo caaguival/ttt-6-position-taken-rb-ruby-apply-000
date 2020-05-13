8c3fac17d03e9bb6402a5bda8896c4d8ea276430
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-13 03:06:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] != "X" || "O"
-    true
-  else board[index] == " " || "" || nil
     !true
+  else board[index] == " " || "" || nil
+    true
 end
 end
